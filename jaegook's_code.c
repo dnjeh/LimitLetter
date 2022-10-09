2888806840d8b80962f7475937eb3686584a3f4e
dnjeh <ehdnj.001@gmail.com>
2022-10-09 21:06:17 +0900

    Today's uncompleted letters'

---
+++ jaegook's_code.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <windows.h>
+#include <io.h>
+
+
+//main
+int main(){
+    //value
+    printf("");
+    int min, max, i;
+    char yn;
+    char name[100];
+    //scanf
+    printf("please input file name and minimum question number and maximum question number(file name example: name.cpp)\n");
+    scanf("%s %d %d", &name, &min ,&max);
+
+    
+
+    //문제 검사
+    if (min > max){
+        printf("The minimum question number cannot be greater than the maximum question number.");
+        return 0;
+    }
+
+    if((access(name, 0) != -1)){ //파일?존재?여부?체크????
+        printf("The file already exists. initialize? (y/n)\n");
+        fflush(stdin);
+        scanf("%c", &yn);
+        if (yn == 'y' || yn == 'Y') {
+            printf("initialize and writing");
+        } 
+        else if(yn == 'n' || yn == 'N') {
+            printf("stop writing");
+            return 0;
+        }
+    }
+
+    FILE* fp = fopen( name , "w");
+    //정상 확인
+    if (fp == NULL) {
+        printf("file cannot be created.");
+    } 
+    else {   
+        //작성
+        fprintf(fp, "#include <stdio.h>");
+        for (i = min; i <= max; i++) {
+            fprintf(fp, "\n\nvoid exam%d(){\n\n}", i);
+
+        }
+        fprintf(fp, "\n\n\n\n\nmain(){\n	int num;\n	while(1){\n    	printf(\"\\n문제 번호: \");\n		scanf(\"%%d\", &num);\n		printf(\"%%d번 문제========================================================================\\n\", num);\n		switch(num){");
+        for (i = min; i <= max; i++) {
+            fprintf(fp, "\n			case %d: exam%d(); break;", i, i);
+        }
+        fprintf(fp, "\n            default: return 0 ;\n		}\n	}\n}");
+
+        printf("successfully created\nmade by msg|ide: vsc|lan: C/CPP");
+        fclose(fp);
+    }
+}
--- letter_v1_1.c
+++ letter_v1_1.c
@@ -1,11 +1,12 @@
 #include <stdio.h>   //177 50
 #include <conio.h>   //94 **
+#include <io.h>
 #include <time.h>    //64 20
 #include <windows.h>
 #include <string.h>
-int i, j, lselect=1, bre;
-int time_vaild();void gotoxy(int x, int y);void CursorView();
-void CursorView(int a);void drawbody();void beforerand();void drawletter();
+int i, j, lselect=1, bre=0, bre2=0;
+int time_vaild();void gotoxy(int x, int y);void CursorView(); void RemoveEnter(char *sentense); void drawmain();
+void CursorView(int a);void drawbody();void beforerand();void drawletter();void SSleep(int a_second);
 int letter_limit[3][4] ={{2022, 9, 25, 21}, {2022, 9, 11, 17}, {0, 0, 0, 0}}; //유효기간 입력
 
 int main(){
@@ -13,48 +14,16 @@ int main(){
     system("chcp 65001");
     system("cls");
     int k;
-    char  *letter_top    = {"홍길동에게"};
-    char  *letter_bottom = {"2022.09.25, 박리다매가"};
-    char  *letter_ps     = {"추신 : "};
-    char  letter_title[20];
-    int letter_mainc = 4; //문단 수
-    char  *letter_main[4][8] = { { //첫 문단
-    "d",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    ""
-    },{                              //둘째 문단
-    "s",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    ""
-    },{                              //셋째 문단
-    "a",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    ""
-    },{                              //넷째 문단
-    "f",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    ""
-    } };
+    char  letter_top[31];
+    char  letter_bottom[31];
+    char  letter_ps[61];
+    char  letter_title[31];
+    char letter_mainc[3]; //문단 수
+    char  letter_main[4][8][270] = { 
+        {"","","","","","","",""},{"","","","","","","",""},
+        {"","","","","","","",""},{"","","","","","","",""} 
+    };
+    FILE *fp;
     char  *command;
     time_t t;
     t = time(NULL);
@@ -65,47 +34,260 @@ int main(){
     letter_limit[2][2] = monthandday->tm_mday;
     letter_limit[2][3] = monthandday->tm_hour;
     CursorView(0);
-    drawbody();
-    drawletter();
-    while(!bre) {
-        gotoxy(56, 32);
-        printf("|                 편지 열기          편지 쓰기                 |");
-        if(lselect) gotoxy(71, 32);
-        else gotoxy(90, 32);
-        printf("√");
-        switch(getch()) {
-            case 'd': case 'a':
-                if(lselect==0) lselect=1;
-                else lselect=0;
-                break;
-            case ' ':
+    while(!bre2) {
+        drawbody();
+        drawletter();
+        while(!bre) {
+            gotoxy(56, 32);
+            printf("|                 편지 열기          편지 쓰기                 |");
+            if(lselect) gotoxy(71, 32);
+            else gotoxy(90, 32);
+            printf("√");
+            switch(getch()) {
+                case 'd': case 'a':
+                    if(lselect==0) lselect=1;
+                    else lselect=0;
+                    break;
+                case ' ':
+                    bre=1;
+                    break;
+                default:
+                    break;
+            }
+        }
+        bre=0;
+        CursorView(1);
+        while(!bre) {
+            gotoxy(56, 32);
+            printf("| 편지의 이름 (30byte 이하) :                                ");
+            gotoxy(86, 32);
+            scanf("%s", letter_title);
+            if((access(letter_title, 0) == -1)&&lselect) {
+                CursorView(0);
+                gotoxy(57, 32);
+                printf("  편지가 존재하지 않습니다(이름을 잘못 적으신게 아니신가요?)");
+                SSleep(3);
+            }
+            else if((access(letter_title, 0)!=-1)&&(lselect==0)) {
+                CursorView(0);
+                gotoxy(57, 32);
+                printf("                   이미 편지가 존재합니다");
+                SSleep(3);
+            }
+            else {
                 bre=1;
-                break;
-            default:
-                break;
+            }
+        }
+        CursorView(0);
+        drawletter();
+        for(i=13;i<16;i++) {
+            gotoxy(56, 15+i);
+            printf("|                                                              |");
+        }
+        if(lselect) {
+            fp=fopen(letter_title, "r");
+            fgets( letter_top, sizeof(letter_top), fp);
+            fgets( letter_bottom, sizeof(letter_bottom), fp);
+            fgets( letter_ps, sizeof(letter_ps), fp);
+            RemoveEnter(letter_top);
+            RemoveEnter(letter_bottom);
+            RemoveEnter(letter_ps);
+            gotoxy(57, 29);
+            printf("     %s에게                  ", letter_top);
+            gotoxy(57, 31);
+            printf("                         %d.%02d.%02d, %s 가     ", letter_limit[2][0], letter_limit[2][1], letter_limit[2][2], letter_bottom);
+            gotoxy(57, 32);
+            printf("         [읽기를 시작하시려면 space bar를 눌러주세요]");
+            switch(getch()) {
+                case ' ':
+                    bre2=1;
+                    break;
+                default:
+                    break;
+            }
+        }
+        else {
+            CursorView(1);
+            gotoxy(57, 27);
+            printf(" 편지 파일의 이름 : %s", letter_title);
+            gotoxy(57, 29);
+            printf(" 받는 사람의 이름 : ");
+            fflush(stdin);
+            fgets(letter_top, 30, stdin);
+            RemoveEnter(letter_top);
+            gotoxy(57, 29);
+            printf("     %s에게                  ", letter_top);
+            gotoxy(57, 31);
+            printf(" 보내는 사람의 이름 : %d.%02d.%02d, ",letter_limit[2][0], letter_limit[2][1], letter_limit[2][2]);
+            fflush(stdin);
+            fgets(letter_bottom, 30, stdin);
+            RemoveEnter(letter_bottom);
+            gotoxy(57, 31);
+            printf("                         %d.%02d.%02d, %s 가     ", letter_limit[2][0], letter_limit[2][1], letter_limit[2][2], letter_bottom);
+            gotoxy(57, 32);
+            printf("         [작성을 시작하시려면 space bar를 눌러주세요]");
+            switch(getch()) {
+                case ' ':
+                    bre2=1;
+                    break;
+                default:
+                    break;
+            }
+            CursorView(0);
         }
     }
-    CursorView(1);
-    gotoxy(56, 32);
-    printf("| 편지의 이름 (20byte 이내) :                          ");
-    gotoxy(86, 32);
-    fgets(letter_title, 20, stdin);
-}
+    CursorView(0);
+    drawbody();
+    drawmain();
+    if(lselect) {
+        fgets(letter_mainc, sizeof(letter_mainc), fp);
+        RemoveEnter(letter_mainc);
+        letter_mainc[0]-=48;
+        getch();
+        for(i=0;i<(int)letter_mainc[0];i++) {
+            for(j=0;j<8;j++) {
+                fgets(letter_main[i][j], sizeof(letter_main[i][j]), fp);
+                RemoveEnter(letter_main[i][j]);
+            }
+        }
+        fclose(fp);
+        for(i=0;i<(int)letter_mainc[0];i++) {
+            CursorView(0);
+            for(j=0;j<8;j++) {
+                gotoxy(44, 2+i*9+j);
+                printf("%s", letter_main[i][j]);
+                SSleep(1);
+            }
+            CursorView(1);
+            getch();
+        }
+    }
+    else {
+        gotoxy(44, 2);
+        printf("작성할 문단의 갯수 : ");
+        scanf("%c", &letter_mainc[0]);
+        fflush(stdin);
+        gotoxy(44, 2);
+        printf(" %s                                                 ", letter_top);
+        letter_mainc[0]-=48;
+        CursorView(1);
+        for(i=0;i<(int)letter_mainc[0];i++) {
+            for(j=0;j<8;j++) {
+                gotoxy(44, 3+i*9+j);
+                fgets(letter_main[i][j], sizeof(letter_main[i][j]), stdin);
+                fflush(stdin);
+            }
+        }
+        fp=fopen(letter_title , "w");
+        fprintf(fp, "%s\n", letter_top);
+        fprintf(fp, "%s\n", letter_bottom);
+        fprintf(fp, "%s\n", letter_ps);
+        fprintf(fp, "%d\n", letter_mainc[0]);
+        for(i=0;i<(int)letter_mainc[0];i++) {
+            for(j=0;j<8;j++) {
+                fprintf(fp, "%s\n", letter_main[i][j]);
+            }
+        }
+        fclose(fp);
+        gotoxy(66, 45);
+        printf("편지 저장이 완료되었습니다!");
+        getch();
+    }
+}   
 
 
 void beforerand() {
     srand(letter_limit[0][0]+letter_limit[0][1]+letter_limit[0][2]+letter_limit[0][3]+letter_limit[1][0]+letter_limit[1][1]+letter_limit[1][2]+letter_limit[1][3]);
 }
-
 void gotoxy(int x,int y) { //gotoxy함수 
     COORD pos={x,y};
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),pos);
+}
+void SSleep(int a_second) {
+    clock_t start_clk = clock();  // 시작 시간을 구한다.
+    a_second--;
+    while (1) {                  // 지속적으로 clock 함수를 호출하여 흘러간 시간을 계산한다.
+        if ((clock() - start_clk) / CLOCKS_PER_SEC > a_second) break;
+    }
 }
 void CursorView(int a) {
     CONSOLE_CURSOR_INFO cursorInfo = { 0, };
     cursorInfo.dwSize = 1; //커서 굵기 (1 ~ 100)
     cursorInfo.bVisible = a?TRUE:FALSE; //커서 Visible TRUE(보임) FALSE(숨김)
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+}
+void RemoveEnter(char *sentence) {
+    for(i=0;sentence[i]!='\0';i++) {
+        if(sentence[i+1]=='\0') sentence[i]=' ';
+    }
+}
+void drawletter() {
+    for(i=0;i<20;i++) {
+        gotoxy(56, 15+i);
+        switch(i) {
+            case 0:  printf(" ______________________________________________________________ "); break;
+            case 1:  printf("| _                                                          _ |"); break;
+            case 2:  printf("|   _                                                      _   |"); break;
+            case 3:  printf("|     _                                                  _     |"); break;
+            case 4:  printf("|       _                                              _       |"); break;
+            case 5:  printf("|         _                                          _         |"); break;
+            case 6:  printf("|           _                                      _           |"); break;
+            case 7:  printf("|             _                                  _             |"); break;
+            case 8:  printf("|                _                            _                |"); break;
+            case 9:  printf("|                   _                      _                   |"); break;
+            case 10: printf("|                       _              _                       |"); break;
+            case 11: printf("|                          __________                          |"); break;
+            case 12: printf("|                                                              |"); break;
+            case 13: printf("|            __,        _|_         _  _  _    _  _            |"); break;
+            case 14: printf("|           /  |  |   |  |  |   |  / |/ |/ |  / |/ |           |"); break;
+            case 15: printf("|           \\_/|_/ \\_/|_/|_/ \\_/|_/  |  |  |_/  |  |_/         |"); break;
+            case 16: printf("|                                                              |"); break;
+            case 17: printf("|                                                              |"); break;
+            case 18: printf("|                                                              |"); break;
+            case 19: printf(" -------------------------------------------------------------- "); break;
+        }
+    }
+}
+void drawmain() {
+    for(i=0;i<46;i++) {
+        gotoxy(42, 1+i);
+        switch(i) {
+            case 0: case 45: printf(" ____________________________________________________________________________________________ "); break;
+            default:  printf("/ %90s /", " "); break;
+        }
+    }
+}
+int time_vaild() {
+    long long int letter_vaild[3];
+    int bre=1;
+    for(i=0;i<3;i++) {
+        letter_vaild[i] = letter_limit[i][0]*365*24+letter_limit[i][2]*24+letter_limit[i][3];
+        bre=1;
+        while(bre) {
+            switch(letter_limit[i][1]) {
+                case 12: case 10: case 8: case 7: case 5: case 3: case 1:
+                    letter_vaild[i]+=31*24;
+                    break;
+                case 11: case 9: case 6: case 4:
+                    letter_vaild[i]+=30*24;
+                    break;
+                case 2:
+                    if((letter_limit[i][0]%4==0&&letter_limit[i][0]%100!=0)||letter_limit[i][0]%400==0) {
+                        letter_vaild[i]+=29*24;
+                    }
+                    else {
+                        letter_vaild[i]+=28*24;
+                    }
+                    break;
+                default:
+                    bre=0;
+                    break;
+            }
+            letter_limit[i][1]--;
+        }
+    }
+    if(letter_vaild[0]>=letter_vaild[2]&&letter_vaild[1]<=letter_vaild[2]) return 1;
+    else return 0;
 }
 void drawbody() {
     gotoxy(0, 0);
@@ -159,63 +341,4 @@ void drawbody() {
     printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀");
     printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀");
     printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀");
-}
-void drawletter() {
-    for(i=0;i<20;i++) {
-        gotoxy(56, 15+i);
-        switch(i) {
-            case 0:  printf(" ______________________________________________________________ "); break;
-            case 1:  printf("| _                                                          _ |"); break;
-            case 2:  printf("|   _                                                      _   |"); break;
-            case 3:  printf("|     _                                                  _     |"); break;
-            case 4:  printf("|       _                                              _       |"); break;
-            case 5:  printf("|         _                                          _         |"); break;
-            case 6:  printf("|           _                                      _           |"); break;
-            case 7:  printf("|             _                                  _             |"); break;
-            case 8:  printf("|                _                            _                |"); break;
-            case 9:  printf("|                   _                      _                   |"); break;
-            case 10: printf("|                       _              _                       |"); break;
-            case 11: printf("|                          __________                          |"); break;
-            case 12: printf("|                                                              |"); break;
-            case 13: printf("|            __,        _|_         _  _  _    _  _            |"); break;
-            case 14: printf("|           /  |  |   |  |  |   |  / |/ |/ |  / |/ |           |"); break;
-            case 15: printf("|           \\_/|_/ \\_/|_/|_/ \\_/|_/  |  |  |_/  |  |_/         |"); break;
-            case 16: printf("|                                                              |"); break;
-            case 17: printf("|                                                              |"); break;
-            case 18: printf("|                                                              |"); break;
-            case 19: printf(" -------------------------------------------------------------- "); break;
-        }
-    }
-}
-int time_vaild() {
-    long long int letter_vaild[3];
-    int bre=1;
-    for(i=0;i<3;i++) {
-        letter_vaild[i] = letter_limit[i][0]*365*24+letter_limit[i][2]*24+letter_limit[i][3];
-        bre=1;
-        while(bre) {
-            switch(letter_limit[i][1]) {
-                case 12: case 10: case 8: case 7: case 5: case 3: case 1:
-                    letter_vaild[i]+=31*24;
-                    break;
-                case 11: case 9: case 6: case 4:
-                    letter_vaild[i]+=30*24;
-                    break;
-                case 2:
-                    if((letter_limit[i][0]%4==0&&letter_limit[i][0]%100!=0)||letter_limit[i][0]%400==0) {
-                        letter_vaild[i]+=29*24;
-                    }
-                    else {
-                        letter_vaild[i]+=28*24;
-                    }
-                    break;
-                default:
-                    bre=0;
-                    break;
-            }
-            letter_limit[i][1]--;
-        }
-    }
-    if(letter_vaild[0]>=letter_vaild[2]&&letter_vaild[1]<=letter_vaild[2]) return 1;
-    else return 0;
 }
